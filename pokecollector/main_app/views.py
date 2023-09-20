b050da4bbdd97c7072f1098e6ab868a962576194
lsmith <therealshaunsred@gmail.com>
2023-09-20 14:17:11 -0400

    Added basic routing and stubbed up templates

--- pokecollector/main_app/views.py
+++ pokecollector/main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+pokemon = [
+    {'name': 'Sparky', 'species': 'Pikachu', 'type': 'Electric', 'description': 'Chonky boi go zapp'}
+]
+
 # Create your views here.
+
+def home (request):
+    return render(request, 'home.html')
+
+def about (request):
+    return render(request, 'about.html')
+
+def poke_index (request):
+    return render(request, 'pokemon/index.html', {
+        'pokemon': pokemon
+    })
